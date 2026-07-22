f86c5a9e009c09ad96ccfec6d4f14689e3cac537
Rahul Kumar <rahulkumar90270@gmail.com>
2026-07-22 08:28:58 +0530

    remove serving html from server

--- server/src/app.js
+++ server/src/app.js
@@ -12,33 +12,4 @@ app.use(express.json());
 app.use("/api/uploads", uploadRoutes);
 app.use("/api/videos", videoRoutes);
 
-const fs = require("fs");
-
-// Serve Vite static assets (support both my-react-app/dist and client/dist)
-const myReactAppDist = path.join(__dirname, "../../my-react-app/dist");
-const clientDist = path.join(__dirname, "../../client/dist");
-const clientDistPath = fs.existsSync(myReactAppDist) ? myReactAppDist : clientDist;
-
-app.use(
-  express.static(clientDistPath, {
-    maxAge: "1y",
-    etag: true,
-    lastModified: true,
-    index: false, // Prevent auto-serving index.html for asset paths
-  })
-);
-
-app.use((req, res) => {
-  const indexPath = path.join(clientDistPath, "index.html");
-  if (fs.existsSync(indexPath)) {
-    res.sendFile(indexPath);
-  } else {
-    res
-      .status(404)
-      .send(
-        "Frontend build (index.html) not found. Please run 'npm run build' in my-react-app."
-      );
-  }
-});
-
 module.exports = app;
